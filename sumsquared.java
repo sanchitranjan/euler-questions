f072e22ea17ddc873327700b3578836da4e1fe50
sanchitranjan <42304353+sanchitranjan@users.noreply.github.com>
2018-10-11 07:48:07 -0700

    Update sumsquared.java

--- sumsquared.java
+++ sumsquared.java
@@ -16,14 +16,13 @@ public class sumsquared
 		for(i=1;i<=100;i++)
 		{
 			sum1+=i;
+		 	sum2+=(i*i);
 		}
 		sum1*=sum1;
-		for(i=1;i<=100;i++)
-		{
-			sum2+=(i*i);
-		}
+		
 		diff=sum1-sum2;
 		System.out.println("diff between sum of square of first 100 natural no and square of sum is ="+diff);  
 
 	}
 }
+
